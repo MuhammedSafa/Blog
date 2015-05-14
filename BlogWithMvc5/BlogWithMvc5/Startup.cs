0e1d9fb8a292a94379e126df34d37b2d01e49f12
Muhammed Safa <muhammedsafabicakci@gmail.com>
2015-05-14 09:24:03 +0300

    Blog Start

---
+++ BlogWithMvc5/BlogWithMvc5/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BlogWithMvc5.Startup))]
+namespace BlogWithMvc5
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
